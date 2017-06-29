736ae6083636c554835c3af82ba8342235e88104
Artenuvielle <renem1@gmx.net>
2017-06-29 14:34:27 +0200

    changed copy destination of install script
    now UBT can find the build file

--- install.py
+++ install.py
@@ -8,9 +8,9 @@ else:
     os_build_script = "build_linux.py"
 
 subprocess.call(os_build_script, shell=True, cwd="build")
-if not os.path.exists(os.path.join("..", "ThirdParty")):
-    os.mkdir(os.path.join("..", "ThirdParty"))
-if os.path.exists(os.path.join("..", "ThirdParty", "libprotobuf")):
-    shutil.rmtree(os.path.join("..", "ThirdParty", "libprotobuf"), True)
+if not os.path.exists(os.path.join("..", "Source", "ThirdParty")):
+    os.mkdir(os.path.join("..", "Source", "ThirdParty"))
+if os.path.exists(os.path.join("..", "Source", "ThirdParty", "libprotobuf")):
+    shutil.rmtree(os.path.join("..", "Source", "ThirdParty", "libprotobuf"), True)
 
-shutil.copytree("libprotobuf", os.path.join("..", "ThirdParty", "libprotobuf"))
+shutil.copytree("libprotobuf", os.path.join("..", "Source", "ThirdParty", "libprotobuf"))
